ec8fd36281cc9148049e51d6d4947849b8ef2b09
Студент1 <1@DESKTOP-7RO4MU5>
2022-11-29 11:53:59 +0300

    Добавьте файлы проекта.

---
+++ Extensions/Program.cs
@@ -0,0 +1,107 @@
+﻿using Extensions;
+Console.WriteLine("Menu");
+Console.WriteLine();
+Console.WriteLine("1. Количество букв в слове");
+Console.WriteLine("2. Чёткое, не чёткое");
+Console.WriteLine("3. Номер телефона");
+string menu = Console.ReadLine();
+bool parse = int.TryParse(menu, out var selected);
+if (parse)
+{
+    switch (selected)
+    {
+        case 1:
+            CountLetter();
+            break;
+
+        case 2:
+            EvenOrOdd();
+            break;
+
+        case 3:
+            Number();
+            break;
+    }
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine("Ошибка");
+    Console.WriteLine();
+}
+void CountLetter()
+{
+    Console.WriteLine("Введите слово");
+    string strings = Console.ReadLine();
+    Console.WriteLine("Введите букву");
+    string letter = Console.ReadLine();
+    bool parse = char.TryParse(letter, out var letters);
+    if (parse)
+    {
+        int result = strings.CharCount(letters);
+        if (result >= 3)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Буква встречается часто");
+            Console.WriteLine();
+            return;
+        }
+        else if (result < 3 && result >= 1)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Буква встречается редко");
+            Console.WriteLine();
+            return;
+        }
+        else if (result == 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Буква не встречается");
+            Console.WriteLine();
+            return;
+        }
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("Ошибка");
+        Console.WriteLine();
+    }
+}
+void EvenOrOdd()
+{
+    Console.WriteLine();
+    Console.WriteLine("Введите число");
+    Console.WriteLine();
+    string evenOrOdd = Console.ReadLine();
+    bool parse = int.TryParse(evenOrOdd, out var selected);
+    if (parse)
+    {
+        int result = selected.EvenOrOdd();
+
+        if (result % 2 == 1)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Число нечёткое");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Число чёткое");
+            Console.WriteLine();
+        }
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("Ошибка");
+        Console.WriteLine();
+    }
+}
+void Number()
+{
+    Console.WriteLine("Введите номер телефона");
+    string number = Console.ReadLine();
+    number.Number();
+}
---
+++ Extensions/StringExtension.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Extensions;
+
+public static class StringExtension
+{
+    public static int CharCount(this string strings, char letter)
+    {
+        int count = 0;
+        for (int i = 0; i < strings.Length; i++)
+        {
+            if (strings[i] == letter)
+                count++;
+        }
+        return count;
+    }
+    public static int EvenOrOdd(this int check)
+    {
+        check = check % 2;
+        return check;
+    }
+    public static string Number(this string number)
+    {
+        Regex regex = new Regex(@"[8]\d{10}$");
+        Regex regex1 = new Regex(@"[+7]\d{10}$");
+        if (regex.IsMatch(number))
+        {        
+            Console.WriteLine();
+            Console.WriteLine("Номер введён некорректно");
+            Console.WriteLine(number.Replace('8','7'));
+            Console.WriteLine();
+            return number;
+        }
+        else if (regex1.IsMatch(number))
+        {
+            number = number.Replace('+', ' ');
+            var i = number.ToList();
+            if (i.First() == '7')
+            {
+                i[0] = '8';
+            }
+            number = i.ToString();
+            Console.WriteLine();
+            Console.WriteLine("Номер введён некорректно");
+            Console.WriteLine(number);
+            Console.WriteLine();
+            return number;
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Номер введён корректно");
+            Console.WriteLine();
+        }
+        return number;
+    }
+}
